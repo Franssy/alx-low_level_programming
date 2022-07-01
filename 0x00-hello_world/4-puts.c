1973d076d92814729adb497c4d7ed194f791371c
Franssy <onyefrancis98@gmail.com>
2022-06-30 21:23:47 -0700

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
-/
-* main - Entry point
-* Return: Always 0 (sucess)
-*/
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (sucess)
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle,\n");
+
 return (0);
 }
