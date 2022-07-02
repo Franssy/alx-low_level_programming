2f7ab29ed8730393045c9b10c58f81495bb6de97
Franssy <onyefrancis98@gmail.com>
2022-07-01 21:23:03 -0700

    a program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,24 +2,32 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - print the last digit of the number stored in the variant n
+ * main - print all possible combination of two digits
  *
- * Return: 0
+ * Return: 0 if success
  */
 int main(void)
 {
 	int ch;
+	int n;
 
 	for (ch = 48; ch <= 57; ch++)
 	{
+	for (n = 49; n <= 57; n++)
+	{
+	if (n > ch)
+	{
 	putchar(ch);
-	if (ch != 57)
+	putchar(n);
+	if (ch != 56 || n != 57)
 	{
 	putchar(44);
 	putchar(32);
 	}
 	}
-	putchar(10);
+	}
+	}
 
+	putchar(10);
 	return (0);
 }
