cf085bc8ba95a058c4979af07db817aa3533075b
Franssy <onyefrancis98@gmail.com>
2022-07-08 06:37:43 -0700

    Hot fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,18 @@
  */
 void more_numbers(void)
 {
-	int num1, num2;
+int num1, num2;
 
-	for (num1 = 0; num1 < 10; num1++)
-	{
-		for (num2 = 0; num2 < 14; num2++)
-		{
-			if  (num2 > 0)
-			{
-			_putchar((num2 / 10) + '0');
-			}
-			_putchar((num2 % 10) + '0');
-		}
-		_putchar(10);
-	}
+for (num1 = 0; num1 < 10; num1++)
+{
+for (num2 = 0; num2 < 14; num2++)
+{
+if  (num2 > 9)
+{
+_putchar((num2 / 10) + '0');
+}
+_putchar((num2 % 10) + '0');
+}
+_putchar(10);
+}
 }
