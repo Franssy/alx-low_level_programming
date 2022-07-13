74a17076c9230831570e9ade1f72edea21076626
Franssy <onyefrancis98@gmail.com>
2022-07-13 03:25:27 -0700

    a function that swaps the values of two integers.
    x and s

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the values of x and s
+ * @x: pointer 1
+ * @s: pointer 2
+ * Return; 0
+ */
+
+void swap_int(int *x, int *s)
+{
+	*x += *s;
+	*s = *x - *s;
+	*x = *x - *s;
+}
