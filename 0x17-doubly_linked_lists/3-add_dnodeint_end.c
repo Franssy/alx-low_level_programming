d75acbc3b643df6fb2db3b03b093a151fade3dd1
Franssy <onyefrancis98@gmail.com>
2022-10-03 03:04:29 -0700

    updated

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
-	new->n = n;
+	new->n = new;
 	new->next = NULL;
 
 	if (*head == NULL)
@@ -31,6 +31,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		last = last->next;
 	last->next = new;
 	new->prev = last;
-	
+
 	return (new);
 }
